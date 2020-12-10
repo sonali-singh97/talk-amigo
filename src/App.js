2973f56310bcd6212d8a579b7b6fca3c584f41da
Sonali singh <sonali122000@gmail.com>
2020-12-10 19:04:19 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 // import logo from './logo.svg';
+
+import {BrowserRouter } from 'react-router-dom';
+import {Route} from "react-router";
 import './App.css';
 import Loginpage from './pages/Loginpage';
 import SignupPage from './pages/SignupPage';
@@ -6,28 +9,18 @@ import FeedPage from './containers/FeedPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
 
       {/* <Loginpage /> */}
       {/* <SignupPage /> */}
+   <Route path="/"  exact component ={SignupPage} />
+   <Route path="/login" exact component= {Loginpage} />
+ <Route path="/feed" exact component= {FeedPage} />
+  
 
-     <FeedPage />
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
+    </BrowserRouter>
   );
 }
 
