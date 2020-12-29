84c0531cd86af798df87687098ed535156dde05e
Sonali singh <56023695+sonali-singh97@users.noreply.github.com>
2020-12-30 00:23:44 +0530

    new nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,10 +9,10 @@ function Navbar() {
         <div className="container-fluid">
             <nav className="navbar  navbar-expand-lg ">
                 <Link className="navbar-brand" to="/feed"> <img src="images/logo.png" alt="logo" className="logo" /> </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <ul className="navbar-nav ml-auto">
                         <li class="nav-item">
                             <a className="nav-link" href="">
@@ -44,7 +44,25 @@ function Navbar() {
                         </li>
                     </ul>
                 </div>
+
+                
             </nav>
+            {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav> */}
         </div>
     );
 }
--- client/src/containers/CreatePost.js
+++ client/src/containers/CreatePost.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Navbar from "../components/Navbar";
 
 function CreatePost() {
 
-
+  const [title, setTitle] = useState("");
+  const [body , setBody] = useState("");
+  const [img , setImg] = useState("");
 
     return (
         <div className="container-fluid stardust-bg">
@@ -19,17 +21,17 @@ function CreatePost() {
 
                                 <div className="form-group text-left  ">
                                     <input type="text" className="form-control create-post-input" id="title"
-                                        placeholder="title" />
+                                        placeholder="title" value= {title} onChange={(e)=>setTitle(e.target.value)} />
                                 </div>
 
                                 <div className="form-group text-left  ">
                                     <input type="textarea" className="form-control create-post-input" id="body"
-                                        placeholder="body" />
+                                        placeholder="body" value={body} onChange={(e)=> setBody(e.target.value)} />
                                 </div>
 
                                 <div className="form-group text-left  ">
                                     <label>Upload Image: </label>
-                                    <input type="file" />
+                                    <input type="file"  value={img} onChange={(e)=>console.log(e.target.files[0])} />
                                 </div>
 
                                 <button type="submit" className="btn btn-lg  login-register-button">
