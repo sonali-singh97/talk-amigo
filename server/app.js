5198ec1581fe62a5cc754819559b15a71d5780c6
Sonali singh <56023695+sonali-singh97@users.noreply.github.com>
2021-01-04 21:42:43 +0530

    move production code

--- server/app.js
+++ server/app.js
@@ -73,12 +73,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 
 module.exports = app;
