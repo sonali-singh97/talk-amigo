edd7bd89d2efe5aafeb37c0ba633d0217007d430
Sonali singh <56023695+sonali-singh97@users.noreply.github.com>
2021-01-05 17:07:28 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -74,5 +74,12 @@ app.use(function(err, req, res, next) {
 });
 
 
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static(path.join(__dirname, "client", "build")))
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 module.exports = app;
